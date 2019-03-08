b1f24de68e68ac8139e7653c9584d68d5cd57df2
Martin Navarrete Villegas <martin.nv6@gmail.com>
2019-03-07 16:58:25 -0800

    Add project files.

---
+++ Delegate Chalenge/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Delegate_Chalenge
+{
+    class Program
+    {
+        delegate double MyDelegate(double price);
+        static void Main(string[] args)
+        {
+            MyDelegate f1;
+            string zone = "";
+            double price = 0;
+            do
+            {
+                Console.WriteLine("Select the zone: ");
+                zone = Console.ReadLine();
+                Console.Write("What's the product price: ");
+                price = Convert.ToDouble(Console.ReadLine());
+                ShipingFees.CalculateFee(zone, ref price);
+                Console.WriteLine("The shipping fee is: " + price);
+            } while (zone != "exit");
+
+        }
+
+        
+
+        
+    }
+}
---
+++ Delegate Chalenge/ShipingFees.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Delegate_Chalenge
+{
+    public abstract class ShipingFees
+    {
+        delegate double MyDelegate(double price);
+
+        public static void CalculateFee(string zone, ref double price)
+        {
+            MyDelegate f1;
+            switch (zone)
+            {
+                case "zone1":
+                    f1 = Zone1;
+                    price = f1(price);
+                    break;
+                case "zone2":
+                    f1 = Zone2;
+                    price = f1(price);
+                    break;
+                case "zone3":
+                    f1 = Zone3;
+                    price = f1(price);
+                    break;
+                case "zone4":
+                    f1 = Zone4;
+                    price = f1(price);
+                    break;
+
+            }
+
+
+        }
+        static double Zone1(double price)
+        {
+            return (price * 0.25);
+        }
+        static double Zone2(double price)
+        {
+            return (price * 0.12 + 20);
+        }
+        static double Zone3(double price)
+        {
+            return (price * .08);
+        }
+        static double Zone4(double price)
+        {
+            return (price * 0.04 + 20);
+        }
+    }
+}
