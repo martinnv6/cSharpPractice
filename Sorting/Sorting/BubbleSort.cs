c592f59db5b665509b10cdfd1fb18448b328b97f
Martin Navarrete Villegas <martin.nv6@gmail.com>
2019-03-11 11:20:10 -0700

    Creating Sorting solutions, Bubble Sort added

---
+++ Sorting/Sorting/BubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sorting
+{
+    public static class BubbleSort
+    {
+        public static int IterationsCounter;
+        public static int[] Sort(int[] set)
+        {
+
+            for (int i = 1; i < set.Length; i++)
+            {
+
+                for (int j = 0; j < set.Length-1; j++)
+                {
+                    if (set[j] > set[j+1])
+                    {
+                        int temp = set[j + 1];
+                        set[j + 1] = set[j];
+                        set[j] = temp;
+                    }
+                    Console.WriteLine($"Iteration {IterationsCounter + 1 }: {String.Join(",", set)}");
+                    IterationsCounter++;
+                }
+            }
+            return null;
+
+        }
+    }
+}
---
+++ Sorting/Sorting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] set = new[] {7, 3, 5, 8, 1, 9, 4, 22, 55, 11, 12, 67};
+
+            //BubbleSort
+            Console.WriteLine($"\n\n-----------------BubbleSort------------------\n\n");
+            Console.WriteLine($"Original Array: {String.Join(",", set)}");
+            BubbleSort.Sort(set);
+            Console.WriteLine("Number of Iterations {0}",BubbleSort.IterationsCounter);
+            Console.ReadKey();
+
+            
+            //QuickSort
+            Console.WriteLine($"\n\n-----------------BubbleSort------------------\n\n");
+            Console.WriteLine($"Original Array: {String.Join(",", set)}");
+            BubbleSort.Sort(set);
+            Console.WriteLine("Number of Iterations {0}", BubbleSort.IterationsCounter);
+            Console.ReadKey();
+
+        }
+    }
+}
